3429885a5a31876b77e2c17ea8c764848021e454
chejel <chejenj@gmail.com>
2023-07-17 14:23:12 -0400

    Add fav icon

--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<img src="./../../images/sig-header.png" width="400" alt="signature" />
+<img src="./../../images/sig-header.png" width="400" class="sig-img" alt="signature" />
 <h2 class="portfolio">Portfolio</h2>
 <br />
 <a href="https://chejel.github.io/data-blog/"><i class="fa-regular fa-pen-to-square fa-lg" /></a>&nbsp;&nbsp;&nbsp;<a
@@ -10,7 +10,7 @@
 <br /><br />
 
 <p class="intro">I create visuals with the aim of making data more understandable and personal.</p>
-<br />I use:
+<p class="focus">My areas of focus:</p>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;700&display=swap");
@@ -18,7 +18,24 @@
   h2.portfolio {
     font-family: "Outfit", sans-serif;
     color: #2a2f4f;
-    font-weight: 700;
+    font-weight: 500;
     text-transform: uppercase;
+  }
+
+  p:not(:last-child) {
+    padding-bottom: 2em;
+  }
+
+  p.focus {
+    font-family: "Outfit", sans-serif;
+    font-size: 0.9rem;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+
+  @media (max-width: 450px) {
+    .sig-img {
+      max-width: 90%;
+    }
   }
 </style>
