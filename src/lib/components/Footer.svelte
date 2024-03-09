ed9a47950dacd4e2fa255b88811c9c6ac88ebf3e
chejel <chejenj@gmail.com>
2024-03-09 00:36:23 -0500

    Add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,12 @@
+<script>
+	// Icons
+	import githubLogo from '$lib/images/github-logo.svg';
+	import observableLogo from '$lib/images/observable-logo.svg';
+	import MailIcon from '$lib/components/MailIcon.svelte';
+</script>
+
+<a href="https://github.com/chejel/"><img src={githubLogo} alt="GitHub logo" width="25" /></a>
+<a href="https://observablehq.com/user/@jeche"
+	><img src={observableLogo} alt="Observable logo" width="25" /></a
+>
+<a href="mailto:chejenj@gmail.com"><MailIcon /></a>
