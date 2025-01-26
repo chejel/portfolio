5d2b7679947292881464ea93e9480d7383603052
chejel <chejenj@gmail.com>
2025-01-26 00:46:03 -0500

    Add project in progress

--- src/lib/data/work.js
+++ src/lib/data/work.js
@@ -1,11 +1,22 @@
 export const data = [
 	{
+		link: '',
+		imgLink: './../../images/mvoc-map-ip.png',
+		imgAlt: 'Screenshot of NYC area map showing media outlet locations',
+		title: 'Many Voices, One City Map',
+		description: `<strong>Interactive map</strong> showcasing community media outlets in the New York City metro area.`,
+		org: `<a href='https://www.journalism.cuny.edu/centers/center-community-media/'>Center for Community Media</a>`,
+		date: '2024-05-26',
+		dateMonthYr: 'In progress',
+		type: 'work',
+		tags: ['Mapbox', 'Svelte']
+	},
+	{
 		link: 'https://drive.google.com/file/d/1UaX5WkYObadeWos2NcJ3HGjyMCClR0za/view?usp=drive_link',
 		imgLink: './../../images/ami-report.png',
 		imgAlt: 'Screenshots of select pages from the AMI report',
 		title: 'AAPI News Media: Origins and Futures',
-		description:
-			'Designed the PDF for this report on the evolution of the news media ecosystem for Asian Americans and Pacific Islanders.',
+		description: `<strong>PDF</strong> for report on the evolution of the news media ecosystem for Asian Americans and Pacific Islanders.`,
 		org: `<a href='https://www.journalism.cuny.edu/centers/center-community-media/'>Center for Community Media</a>`,
 		date: '2024-05-26',
 		dateMonthYr: 'May 2024',
@@ -17,8 +28,7 @@ export const data = [
 		imgLink: './../../images/intl-cities.png',
 		imgAlt: 'Screenshots of global map and sidebar',
 		title: 'International City Names in the U.S.',
-		description:
-			'An interactive map that lets you select a city to see the locations of places in the U.S. that share the name.',
+		description: `An <strong>interactive map</strong> that lets you select a city to see the locations of places in the U.S. that share the name.`,
 		org: 'Personal project',
 		date: '2024-02-15',
 		dateMonthYr: 'Feb 2024',
@@ -30,8 +40,7 @@ export const data = [
 		imgLink: './../../images/bmgb.png',
 		imgAlt: 'Screenshots of lollipop chart and card graphics from the Black Media Green Book',
 		title: 'Black Media Green Book',
-		description:
-			"Charts and graphics to visualize <a href='https://bmigreenbook.journalism.cuny.edu/tech-stack/survey-findings/'>survey findings</a> and <a href='https://bmigreenbook.journalism.cuny.edu/tech-stack/case-studies/'>case studies</a> for the Black Media Green Book, a resource to connect Black media outlets with freelancers and experts, and give insights into technology access and use.",
+		description: `<strong>Charts and graphics</strong> to visualize <a href='https://bmigreenbook.journalism.cuny.edu/tech-stack/survey-findings/'>survey findings</a> and <a href='https://bmigreenbook.journalism.cuny.edu/tech-stack/case-studies/'>case studies</a> for the Black Media Green Book, a resource to connect Black media outlets with freelancers and experts, and give insights into technology access and use.`,
 		org: "<a href='https://www.journalism.cuny.edu/centers/center-community-media/'>Center for Community Media</a>",
 		dateMonthYr: 'Oct 2023',
 		date: '2023-10-19',
@@ -43,8 +52,7 @@ export const data = [
 		imgLink: './../../images/aapi-media-map.png',
 		imgAlt: 'Screenshots of the AAPI Media Map and sidebar',
 		title: 'AAPI Media Map',
-		description:
-			'Interactive map of media outlets serving Asian Americans and Pacific Islanders. Includes data tables resulting from applying community, language, format and location filters.',
+		description: `<strong>Interactive map</strong> of media outlets serving Asian Americans and Pacific Islanders.`,
 		org: "<a href='https://www.journalism.cuny.edu/centers/center-community-media/'>Center for Community Media</a>",
 		dateMonthYr: 'Dec 2023',
 		date: '2023-12-08',
@@ -56,8 +64,7 @@ export const data = [
 		imgLink: './../../images/aapi-media-vis.png',
 		imgAlt: 'Screenshots of map and bubble charts showcasing the AAPI media landscape',
 		title: 'Visualizing the AAPI media landscape',
-		description:
-			'Visualizations to showcase the scope and diversity of the Asian American and Pacific Islander media landscape. Accompanies the AAPI Media Directory.',
+		description: `<strong>Visualizations</strong> to showcase the scope and diversity of the Asian American and Pacific Islander media landscape. Accompanies the AAPI Media Directory.`,
 		org: "<a href='https://www.journalism.cuny.edu/centers/center-community-media/'>Center for Community Media</a>",
 		dateMonthYr: 'Dec 2023',
 		date: '2023-12-08',
@@ -70,8 +77,7 @@ export const data = [
 		imgAlt:
 			'Screenshots of bar and bubble charts made for a study of New York state agency advertising practices.',
 		title: 'Community Blindspot',
-		description:
-			"Bubble and bar charts for study of New York State agency advertising practices, including the <a href='https://abi.journalism.cuny.edu/nys-ad-spending/gaming-commission/'>Gaming Commission</a> and <a href='https://abi.journalism.cuny.edu/nys-ad-spending/children-family-services/'>Children and Family Services</a>.",
+		description: `<strong>Bubble and bar charts</strong> for study of New York State agency advertising practices, including the <a href='https://abi.journalism.cuny.edu/nys-ad-spending/gaming-commission/'>Gaming Commission</a> and <a href='https://abi.journalism.cuny.edu/nys-ad-spending/children-family-services/'>Children and Family Services</a>.`,
 		org: "<a href='https://www.journalism.cuny.edu/centers/center-community-media/'>Center for Community Media</a>",
 		date: '2023-09-19',
 		dateMonthYr: 'Sept 2023',
@@ -83,8 +89,7 @@ export const data = [
 		imgLink: './../../images/latino-media.png',
 		imgAlt: 'Screenshots of waffle, bar and line charts for Latino media report',
 		title: 'News Agenda of Latino Media',
-		description:
-			"Charts made for content analysis report on Spanish-language media coverage in the U.S., including on <a href='https://latinomediacontent.journalism.cuny.edu/en/immigrants-hispanics-latinos/'>terminology</a>, <a href='https://latinomediacontent.journalism.cuny.edu/en/immigration-work-health/'>immigration coverage</a>, <a href='https://latinomediacontent.journalism.cuny.edu/en/racism/'>racism</a> and <a href='https://latinomediacontent.journalism.cuny.edu/en/increased-attention-democratic-party/'>political parties</a>.",
+		description: `<strong>Charts</strong> made for content analysis report on Spanish-language media coverage in the U.S., including on <a href='https://latinomediacontent.journalism.cuny.edu/en/immigrants-hispanics-latinos/'>terminology</a>, <a href='https://latinomediacontent.journalism.cuny.edu/en/immigration-work-health/'>immigration coverage</a>, <a href='https://latinomediacontent.journalism.cuny.edu/en/racism/'>racism</a> and <a href='https://latinomediacontent.journalism.cuny.edu/en/increased-attention-democratic-party/'>political parties</a>.`,
 		org: "<a href='https://www.journalism.cuny.edu/centers/center-community-media/'>Center for Community Media</a>",
 		date: '2020-08-18',
 		dateMonthYr: 'Aug 2020',
